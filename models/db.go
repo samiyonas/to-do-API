52fcf246ad6a4e4d7f5bf1843de9a6892d4f12f9
samiyonas <samuel.x.yonas@gmail.com>
2024-10-29 02:57:02 -0700

    models/db.go created table creater functions

--- models/db.go
+++ models/db.go
@@ -2,21 +2,51 @@ package models
 
 import (
     "fmt"
+    "errors"
     "log"
     "database/sql"
     "os"
     _ "github.com/go-sql-driver/mysql"
 )
 
-func init_db() (*sql.DB) {
+func init_db() (*sql.DB, error) {
     username := os.Getenv("DB_USER")
     password := os.Getenv("DB_PASSWD")
     database := os.Getenv("DB")
     dsn := fmt.Sprintf("%s:%s@tcp(localhost:8080)/%s", username, password, database)
     db, err := sql.Open("mysql", dsn)
     if err != nil {
-        log.Fatalf("Error creating the database pointer", err)
+        return db, errors.New("Error creating the database pointer")
     }
 
-    return db
+    return db, nil
+}
+
+func create_tables(db *sql.DB) (error) {
+    user_table := `CREATE TABLE IF NOT EXISTS user (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        email VARCHAR(100) NOT NULL
+    )`
+
+    task_table := `CREATE TABLE IF NOT EXISTS todo (
+        user_id INT,
+        FOREIGN KEY(user_id) REFERENCES user(id),
+        id INT AUTO_INCREMENT UNIQUE,
+        title VARCHAR(256),
+        content VARCHAR(1024),
+        done TINYINT(1) NOT NULL DEFAULT 0
+    )`
+
+    _, err := db.Exec(user_table)
+    if err != nil {
+        return errors.New("error creating table")
+    }
+
+    _, err = db.Exec(task_table)
+    if err != nil {
+        return errors.New("error creating table")
+    }
+
+    return nil
 }
