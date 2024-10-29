77bc3fb66cd70f8141a13ab94076c05129b6fd1c
samiyonas <samuel.x.yonas@gmail.com>
2024-10-29 06:33:59 -0700

    made some changes to the schema

--- models/db.go
+++ models/db.go
@@ -28,7 +28,7 @@ func create_tables() (error) {
     user_table := `CREATE TABLE IF NOT EXISTS user (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
-        email VARCHAR(100) NOT NULL
+        email VARCHAR(100) NOT NULL, UNIQUE
     )`
 
     task_table := `CREATE TABLE IF NOT EXISTS todo (
