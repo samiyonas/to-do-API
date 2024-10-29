437897a6c8cdd123e730f0372b7eb64585b05a86
samiyonas <samuel.x.yonas@gmail.com>
2024-10-29 06:33:43 -0700

    operations/ addes a user to the database

--- models/operations.go
+++ models/operations.go
@@ -1,8 +1,16 @@
 package models
 
 func add_user(user User) (error) {
-    query := "INSERT INTO (name, email) VALUES (?, ?)"
-    _, err := db.Exec(query, user.name, user.email)
+    query := "SELECT * FROM user WHERE email = ?"
+    result, err = db.Query(query, user.email)
+    if err != nil {
+        return errors.New("this is internal failure")
+    }
+    if result.Next() {
+        return errors.New("email already exists")
+    }
+    cmd := "INSERT INTO (name, email) VALUES (?, ?)"
+    _, err := db.Exec(cmd, user.name, user.email)
     if err != nil {
         return errors.New("not inserted")
     }
