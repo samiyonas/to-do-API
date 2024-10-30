7c3b18b0c4643b33b9a1f386fc0acf252ad039bc
samiyonas <samuel.x.yonas@gmail.com>
2024-10-30 00:31:57 -0700

    fixed naming and exporting issues

--- models/operations.go
+++ models/operations.go
@@ -1,16 +1,21 @@
 package models
 
-func add_user(user User) (error) {
+import (
+    "errors"
+)
+
+func Add_user(user User) (error) {
     query := "SELECT * FROM user WHERE email = ?"
-    result, err = db.Query(query, user.email)
+    result, err := db.Query(query, user.Email)
     if err != nil {
         return errors.New("this is internal failure")
     }
     if result.Next() {
+        result.Close()
         return errors.New("email already exists")
     }
     cmd := "INSERT INTO (name, email) VALUES (?, ?)"
-    _, err := db.Exec(cmd, user.name, user.email)
+    _, err = db.Exec(cmd, user.Name, user.Email)
     if err != nil {
         return errors.New("not inserted")
     }
