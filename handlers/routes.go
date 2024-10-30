673e714d77d582f3b4880a8c688c5cb2f99c47c5
samiyonas <samuel.x.yonas@gmail.com>
2024-10-30 12:55:55 -0700

    added a new route

--- handlers/routes.go
+++ handlers/routes.go
@@ -11,7 +11,7 @@ import (
 )
 
 func Task_by_Id(w http.ResponseWriter, r *http.Request) {
-    task_id := chi.URLParam(r, "userID")
+    task_id := chi.URLParam(r, "taskID")
 
     taskId, err := strconv.Atoi(task_id)
     if err != nil {
@@ -28,6 +28,31 @@ func Task_by_Id(w http.ResponseWriter, r *http.Request) {
     }
 
     jsonData, err := json.Marshal(task)
+    if err != nil {
+        http.Error(w, "couldn't decode your task\n", http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonData)
+}
+
+func All_tasks(w http.ResponseWriter, r *http.Request) {
+    user_id := chi.URLParam(r, "userID")
+
+    userId, err := strconv.Atoi(user_id)
+    if err != nil {
+        http.Error(w, "Error retrieving URL Param\n", http.StatusInternalServerError)
+        return
+    }
+    
+    tasks, err := models.All_tasks(userId)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    jsonData, err := json.Marshal(tasks)
     if err != nil {
         http.Error(w, "couldn't decode your task\n", http.StatusInternalServerError)
         return
