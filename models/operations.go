31f1967b69afc02429430882f14ca98423938f71
samiyonas <samuel.x.yonas@gmail.com>
2024-10-30 02:05:27 -0700

    capitalizing more function

--- models/operations.go
+++ models/operations.go
@@ -6,20 +6,20 @@ import (
 
 func Add_user(user User) (int64, error) {
     query := "SELECT * FROM user WHERE email = ?"
-    result, err := db.Query(query, user.Email)
+    result, err := Db.Query(query, user.Email)
     if err != nil {
-        return errors.New("this is internal failure")
+        return -1, errors.New("this is internal failure")
     }
     if result.Next() {
         result.Close()
-        return errors.New("email already exists")
+        return -1, errors.New("email already exists")
     }
     cmd := "INSERT INTO (name, email) VALUES (?, ?)"
-    info, err = db.Exec(cmd, user.Name, user.Email)
+    info, err := Db.Exec(cmd, user.Name, user.Email)
     if err != nil {
-        return errors.New("not inserted")
+        return -1, errors.New("not inserted")
     }
-    id, err := info.LastInsertedId()
+    id, err := info.LastInsertId()
     if err != nil {
         return -1, errors.New("not the right ID")
     }
@@ -30,14 +30,14 @@ func Add_user(user User) (int64, error) {
 func Add_task(task Task) (int64, error) {
     cmd := "INSERT INTO todo (user_id, title, content, done) VALUES(?, ?, ?, ?)"
 
-    info, err := db.Exec(cmd, task.User_id, task.Title, task.Content, task.Done)
+    info, err := Db.Exec(cmd, task.User_id, task.Title, task.Content, task.Done)
     if err != nil {
-        return errors.New("task not added")
+        return -1, errors.New("task not added")
     }
-    id, err := info.LastInsertedId()
+    id, err := info.LastInsertId()
     if err != nil {
         return -1, errors.New("not the right ID")
     }
 
-    return nil, id
+    return id, nil
 }
