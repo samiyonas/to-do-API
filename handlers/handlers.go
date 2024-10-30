35e530a42437faaa509e33c2761005de23f8e4a1
samiyonas <samuel.x.yonas@gmail.com>
2024-10-30 13:37:49 -0700

    documenting handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,20 +1,23 @@
 package handlers
 
 import (
-    "fmt"
-    "encoding/json"
-    "net/http"
-    "github.com/samiyonas/to-do-API/models"
+    "fmt" // For formatting string
+    "encoding/json" // For encoding and decoding json
+    "net/http" // For handling requests
+    "github.com/samiyonas/to-do-API/models" // To retrieve from database
 )
 
 func Add_user(w http.ResponseWriter, r *http.Request) {
+    // user object
     var user models.User
 
+    // check for method similarity
     if r.Method != http.MethodPost {
         http.Error(w, "Unsupported request method\n", http.StatusMethodNotAllowed)
         return
     }
 
+    // decode the request body into our struct type
     decoder := json.NewDecoder(r.Body)
     defer r.Body.Close()
 
@@ -24,24 +27,30 @@ func Add_user(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    // Add the user to the database
     id, err := models.Add_user(user)
     if err != nil {
         http.Error(w, fmt.Sprintf("couldn't add you\n", err), http.StatusInternalServerError)
         return
     }
 
+    // preparing the response body
     ret := fmt.Sprintf("You've been added and your ID is %d\n", id)
 
+    // Writes it to the response body
     w.Write([]byte(ret))
 }
 
 func Add_task(w http.ResponseWriter, r *http.Request) {
+    // task table object
     var task models.Task
+    // checking for method similarity
     if r.Method != http.MethodPost {
         http.Error(w, "Unsupported request method\n", http.StatusMethodNotAllowed)
         return
     }
 
+    // decoding request body to our type struct
     decoder := json.NewDecoder(r.Body)
     defer r.Body.Close()
 
@@ -51,11 +60,14 @@ func Add_task(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    // Add task to the database
     id, err := models.Add_task(task)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    // prepare the response body
     ret := fmt.Sprintf("Your task has been added! You're task ID is %d\n", id)
+    // write to the response body
     w.Write([]byte(ret))
 }
